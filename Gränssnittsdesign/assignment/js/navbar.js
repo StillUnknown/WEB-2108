2f62548fb11c017250dae144802b38da2774b1df
StillUnknown <knavinglogistic@hotmail.se>
2021-12-22 14:49:41 +0100

    Fortsätter med projektet

--- Gränssnittsdesign/assignment/js/navbar.js
+++ Gränssnittsdesign/assignment/js/navbar.js
@@ -1,9 +1,13 @@
 function navbar() {
     return ` <ul id="navbar">
-        <li class="navbar-item navbar-hover">Categories</li>
-        <li class="navbar-item navbar-hover">Winter Sale</li>
-        <li class="navbar-item navbar-hover">Bestsellers</li>
-        <li class="navbar-item navbar-hover">Contact</li>
+        <li class="navbar-item navbar-hover">
+        <a href="black_orange.html">Black and orange colorscheme</a></li>
+        <li class="navbar-item navbar-hover">
+        <a href="blueish.html">Blue colorscheme</a></li>
+        <li class="navbar-item navbar-hover">
+        <a href="purple.html">Purple colorscheme</a></li>
+        <li class="navbar-item navbar-hover">
+        <a href="red_orange.html">Red and orange colorscheme</a></li>
         <li class="navbar-item"><label for="Håkan Hellström"></label><input type="text" id="Håkan Hellström" value="Search">
         <button onclick="searchButton()">Search</button></li>
         
