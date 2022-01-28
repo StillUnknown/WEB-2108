094a2e423024773a6b6d64430a24013f239a6ac8
StillUnknown <knavinglogistic@hotmail.se>
2022-01-28 13:01:59 +0100

    assignment forts 2022-01-28

--- webbutveckling_2/assignment/assignment/src/components/card/Card.js
+++ webbutveckling_2/assignment/assignment/src/components/card/Card.js
@@ -1,8 +1,10 @@
-const Card = ({ id, title, price }) => {
+import css from './Card.module.css'
+
+const Card = ({ id, image, title, price }) => {
     return (
         <article className='tc bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-            <img src={ `https://robohash.org/${ id }?size=250x250` } alt='Robot'/>
             <div>
+                <img className={css.cdCover} src={image} alt='cdCover'/>
                 <h2>{ title }</h2>
                 <h3>{ price } kr</h3>
                 <button>BUY</button>
--- webbutveckling_2/assignment/assignment/src/utils/data/AlbumsData.js
+++ webbutveckling_2/assignment/assignment/src/utils/data/AlbumsData.js
@@ -1,57 +1,61 @@
 import picOne from '../img/hardstyle-one.PNG'
+import picTwo from '../img/hardstyle-two.PNG'
+import picthree from '../img/hardstyle-three.PNG'
+import picFour from '../img/hardstyle-four.PNG'
+import picFive from '../img/hardstyle-five.PNG'
+import picSix from '../img/hardstyle-six.PNG'
+import picSeven from '../img/hardstyle-seven.PNG'
+import picEight from '../img/HåkanHellström.PNG'
 
 const albumsData = [
     {
         id: 1,
-        image: {picOne},
+        image: picOne,
         title: 'Mixed Artists - Hardstyle 2016',
         price: 229,
     },
     {
         id: 2,
+        image: picTwo,
         title: 'Mixed Artists - Hardstyle 2017',
         price: 259,
     },
     {
         id: 3,
+        image: picthree,
         title: 'Mixed Artists - Hardstyle 2018',
         price: 119,
     },
     {
         id: 4,
+        image: picFour,
         title: 'Mixed Artists - Hardstyle 2019',
         price: 239,
     },
     {
         id: 5,
+        image: picFive,
         title: 'Mixed Artists - Hardstyle 20120',
         price: 219,
     },
     {
         id: 6,
+        image: picSix,
         title: 'Mixed Artists - Hardstyle 2021',
         price: 289,
     },
     {
         id: 7,
+        image: picSeven,
         title: 'Mixed Artists - Hardstyle 2022',
         price: 277,
     },
     {
         id: 8,
+        image: picEight,
         title: 'Håkan Hellström - Illusioner',
         price: 99,
     },
-    {
-        id: 9,
-        title: 'Hej',
-        price: 1,
-    },
-    {
-        id: 10,
-        title: 'Hej Hej',
-        price: 2
-    }
 ]
 
 export default albumsData
