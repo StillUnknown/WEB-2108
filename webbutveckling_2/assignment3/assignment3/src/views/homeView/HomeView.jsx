d53e009715c488c732b2f8fbe1ffd7db4a81428c
StillUnknown <knavinglogistic@hotmail.se>
2022-02-09 22:14:53 +0100

    Fortsätter med css

--- webbutveckling_2/assignment3/assignment3/src/views/homeView/HomeView.jsx
+++ webbutveckling_2/assignment3/assignment3/src/views/homeView/HomeView.jsx
@@ -8,7 +8,7 @@ function HomeView() {
                 Welcome to my webshop. Under "Our albums" you will find some albums we have for sale.
             </p>
             <p>
-                Under "Video" you'll find a surprice!
+                Under "Video" you'll find a surprise!
             </p>
             <p>
                 Under "Helpers" you'll find three people who helped me a lot with this assignment
