e2bcec317eae670d7428799b43bb468cb29dccb2
StillUnknown <knavinglogistic@hotmail.se>
2022-02-17 15:11:02 +0100

    Dagens lektion 2022-02-17

--- Webbserverprogrammering_1/Lektioner/2022-02-17/backend/src/server.js
+++ Webbserverprogrammering_1/Lektioner/2022-02-17/backend/src/server.js
@@ -4,14 +4,73 @@ import cors from 'cors'
 
 dotenv.config()
 const port = process.env.SERVER_PORT
-const app = express()
 
 const cors_options = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
+const app = express()
 
 app.use(cors(cors_options))
+
+const userDatabase = [
+    {
+        name: 'Aram',
+        age: 30,
+        gender: 'Male'
+    },
+    {
+        name: 'Bengtina',
+        age: 19,
+        gender: 'female'
+    },
+    {
+        name: 'Carin',
+        age: 29,
+        gender: 'female'
+    },
+    {
+        name: 'Emil',
+        age: 32,
+        gender: 'Male'
+    }
+]
+
+const userNames = () => {
+    const names = []
+    userDatabase.forEach(user => {
+        names.push({
+            name: user.name
+        })
+    })
+    return names
+}
+
+const getUserByName = (name) => {
+    let object = `Could not find ${name} in database`
+    userDatabase.forEach(user => {
+        if (name === user.name) {
+            object = user
+            return
+        }
+    })
+    return object
+}
+
+app.get('/users', (req, res) => {
+    res.send(userDatabase)
+})
+
+app.get('/users/name', (req, res) => {
+    const responseFromDb = userNames()
+    res.send(responseFromDb)
+})
+
+app.get('/user/:name', (req, res) => {
+    const name = req.params.name
+    const responseFromDb = getUserByName(name)
+    res.send(responseFromDb)
+})
 
 app.get('/', (req, res) => {
     res.send(`API is Alive på port ${port}`)
