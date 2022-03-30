960b1b771d7d694388fe3a3563a536ad8ee4820e
StillUnknown <knavinglogistic@hotmail.se>
2022-03-30 17:00:24 +0200

    Ser till att backenden fungerar

--- Datalagring/assignmentMongoMongoose/backend/src/server.js
+++ Datalagring/assignmentMongoMongoose/backend/src/server.js
@@ -5,18 +5,20 @@ import morgan from 'morgan'
 import Middlewares from "./middlewares/Middlewares.js"
 import Configurations from "./configurations/Configurations.js"
 import TaskRoutes from "./routes/TaskRoutes.js"
-
 import ApplyMiddlewares from "./configurations/ApplyMiddlewares.js"
+import AliveRoute from "./routes/AliveRoute.js";
+
 
 dotenv.config()
 const app = express()
 
-app.use(ApplyMiddlewares.ApplyMiddlewares)
+ApplyMiddlewares.ApplyMiddlewares
 app.use(helmet())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(morgan('common'))
 
-
+AliveRoute.aliveRoute(app)
 TaskRoutes.taskRoutes(app)
 app.use(Middlewares.notFound)
 app.use(Middlewares.errorHandler)
