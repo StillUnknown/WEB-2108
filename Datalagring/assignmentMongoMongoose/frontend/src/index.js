6aa66ee65388af76774ebfee16e0f7b0afef3e46
StillUnknown <knavinglogistic@hotmail.se>
2022-04-01 19:35:19 +0200

    Lägger till frontend react till inlämningen

---
+++ Datalagring/assignmentMongoMongoose/frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './utils/global/style/global.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+reportWebVitals();
