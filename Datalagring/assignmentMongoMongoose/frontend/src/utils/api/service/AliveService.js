e3bc93ce9a1f7f3f2caf0dd4659372d9d936e1b8
StillUnknown <knavinglogistic@hotmail.se>
2022-04-01 20:11:46 +0200

    Börjar bygga routes för frontend

---
+++ Datalagring/assignmentMongoMongoose/frontend/src/utils/api/service/AliveService.js
@@ -0,0 +1,7 @@
+import http from '../TaskAPI'
+
+const alive = () => {
+    return http.get('/')
+}
+
+export default alive
---
+++ Datalagring/assignmentMongoMongoose/frontend/src/utils/api/service/TaskService.js
@@ -0,0 +1,38 @@
+import http from '../TaskAPI'
+
+const taskUrl = `/task`
+const tasksUrlById = `${taskUrl}/:userId`
+const searchTask = `/searchTask`
+
+const getAllTasks = () => {
+    return http.get(taskUrl)
+}
+
+const createTask = () => {
+    return http.post(taskUrl)
+}
+
+const updateTask = (_id) => {
+    return http.put(tasksUrlById)
+}
+
+const deleteTask = (_id) => {
+    return http.delete(tasksUrlById)
+}
+
+const getTaskById = (_id) => {
+    return http.get(tasksUrlById)
+}
+
+const getTaskWithNameQuery = () => {
+    return http.get(searchTask)
+}
+
+export default {
+    getAllTasks,
+    createTask,
+    updateTask,
+    deleteTask,
+    getTaskById,
+    getTaskWithNameQuery
+}
