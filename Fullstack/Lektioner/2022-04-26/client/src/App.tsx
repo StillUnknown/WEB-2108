a854424c54af99e40607b49415ad932a0fb10155
StillUnknown <knavinglogistic@hotmail.se>
2022-04-28 09:59:05 +0200

    Småfix

--- Fullstack/Lektioner/2022-04-26/client/src/App.tsx
+++ Fullstack/Lektioner/2022-04-26/client/src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Users from "./components/users/Users";
 import UserPage from "./components/users/UserPage";
 
 const App: React.FC = () => {
-  return (
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/users" element={<Users />} />
-            <Route path="/users/:id" element={<UserPage />} />
-          </Routes>
-        </Router>
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/users" element={<Users/>}/>
+                    <Route path="/users/:id" element={<UserPage/>}/>
+                </Routes>
+            </Router>
 
-      </div>
-  )
+        </div>
+    )
 }
 
 export default App
