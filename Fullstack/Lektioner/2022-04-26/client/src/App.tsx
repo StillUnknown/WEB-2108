8d3e5a2cc1096fb0d65ac244f398a27de689eb22
StillUnknown <knavinglogistic@hotmail.se>
2022-04-28 09:45:55 +0200

    Börjar bygga frontend

---
+++ Fullstack/Lektioner/2022-04-26/client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Users from "./components/users/Users";
+import UserPage from "./components/users/UserPage";
+
+const App: React.FC = () => {
+  return (
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/:id" element={<UserPage />} />
+          </Routes>
+        </Router>
+
+      </div>
+  )
+}
+
+export default App
